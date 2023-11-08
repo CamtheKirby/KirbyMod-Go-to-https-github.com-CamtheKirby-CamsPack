cedd033721e2ee6e7317c1452ed66ba079dc1195
Cam the Kirby <92703353+CamtheKirby@users.noreply.github.com>
2023-11-08 15:02:08 -0600

    Last Change

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.5.6";
     public const string Name = "Kirby Mod";
 
-    public const string Description = "A mod that adds Kirby characters to BTD6!";
+    public const string Description = "DOWNLOAD CAM'S PACK INSTEAD OF THIS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! A mod that adds Kirby characters to BTD6!";
 
     public const string RepoOwner = "CamtheKirby"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "KirbyMod"; // TODO add your repo name here, also in the download url in README.md
